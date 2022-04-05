352061b54a71e3565eaf90cfb55dfbd4cd900d46
rchan100 <64880162+rchan100@users.noreply.github.com>
2022-04-05 12:12:43 -0700

    Update Q1.asm

--- Q1.asm
+++ Q1.asm
@@ -1,3 +1,15 @@
+; Prompt: Converting from Big Endian to Little Endian
+; 	  Write a program using the variables below and MOV instructions to copy the value 
+;	  from bigEndian to littleEndian, reversing the order of the bytes. The number's 
+;	  32-bit value is understood to be 12345678 hexadecimal
+
+; Given:
+;	.data
+;	bigEndian BYTE 12h, 34h, 56h, 78h
+;	littleEndian DWORD?
+
+; Solution:
+
 .386
 .model flat,stdcall
 .stack 4096
